c9e8e45e9fb1f0ebf94a775d97cd33547a887fb9
jittarin Rojanakhajonrachati <jittarin8857@gmail.com>
2018-06-28 17:52:24 +0700

    Update LCD.java

--- src/main/java/LCD.java
+++ src/main/java/LCD.java
@@ -1,14 +1,13 @@
 public class LCD {
 
-    public String convert(int number) {
+    public String convert(String number) {
         String result1 = "", result2 = "", result3 = "";
-        String numString = number + "";
         String[] line1 = {" _ ", "   ", " _ ", " _ ", "   ", " _ ", " _ ", " _ ", " _ ", " _ "};
         String[] line2 = {"| |", "  |", " _|", " _|", "|_|", "|_ ", "|_ ", "  |", "|_|", "|_|"};
         String[] line3 = {"|_|", "  |", "|_ ", " _|", "  |", " _|", "|_|", "  |", "|_|", " _|"};
 
-        for (int i = 0; i < numString.length(); i++) {
-            int indexNum = Integer.parseInt(numString.substring(i, i + 1));
+        for (int i = 0; i < number.length(); i++) {
+            int indexNum = Integer.parseInt(number.substring(i, i + 1));
             result1 += line1[indexNum];
             result2 += line2[indexNum];
             result3 += line3[indexNum];
