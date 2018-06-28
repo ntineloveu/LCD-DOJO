45ec817d62baeb9207214f9e44b882cf15b48070
jittarin Rojanakhajonrachati <jittarin8857@gmail.com>
2018-06-28 17:54:00 +0700

    Update and rename DoJo.java to MainApp.java

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class DoJo {
+public class MainApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("please enter number");
