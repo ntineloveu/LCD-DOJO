19942eb167dbd12080277bf54289ebad9ac43d61
jittarin Rojanakhajonrachati <jittarin8857@gmail.com>
2018-06-28 17:53:35 +0700

    Update DoJo.java

--- src/main/java/DoJo.java
+++ src/main/java/DoJo.java
@@ -1,6 +1,13 @@
 import java.util.*;
 public class DoJo {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter number");
+        String input = scanner.next();
+
+        LCD lcd = new LCD();
+        String output = lcd.convert(input);
+
+        System.out.println(output);
     }
 }
